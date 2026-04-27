c713557d1bbc1d24d726e639fd74dbe96cf0bbe7
Shreyas0012 <shreyasupadhye494949@gmail.com>
2026-04-27 14:49:59 +0530

    Start

---
+++ src/test/java/com/localvendor/vendorsystem/VendorsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localvendor.vendorsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
